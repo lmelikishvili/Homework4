a80f7373b1de215ad00e7653eb1b9b77acaebe53
lmelikishvili <levanimelikishvili@gmail.com>
2023-11-02 01:12:58 +0400

    asd

--- app/src/main/java/com/example/homework4/MainActivity.kt
+++ app/src/main/java/com/example/homework4/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.homework4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        findViewById<AppCompatTextView>(R.id.textView4).setOnClickListener {
+            findViewById<AppCompatTextView>(R.id.textView3).setText("Left Button pushed!!!")
+        }
+
+
     }
 }
